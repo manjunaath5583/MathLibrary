44fe8ac87105111e29bd515cba1acc7a599f6d44
manjunaath <manjunaath5583@gmail.com>
2023-02-14 00:09:00 +0530

    Added new functions in Vector2: unitvector, magnitude_of_vector, Direction_of_vector, Angle_between_vectors(radians), zerovector

--- includes/vector2.cpp
+++ includes/vector2.cpp
@@ -1,3 +1,4 @@
+#include <math.h>
 #include "Vector2.h"
 
 /*
@@ -52,6 +53,20 @@ double Vector2::operator*(const Vector2 &other)
 }
 
 /*
+    DIVISON OF VECTORS (SCALAR)
+
+    vector a = xi + yj;
+    a/2 = (x/2)i + (y/2)j #VECTOR
+*/
+Vector2 Vector2::operator/(double Magnitude)
+{
+    X_COMPONENT /= Magnitude;
+    Y_COMPONENT /= Magnitude;
+
+    return *this;
+}
+
+/*
     COMPARISON OF VECTORS
     vector a = x1i + y1j
     vector b = x2i + y2j
@@ -69,6 +84,12 @@ bool Vector2::operator==(const Vector2 &other) const
     return false;
 }
 
+bool Vector2::operator!=(const Vector2 &other) const
+{
+
+    return !(Vector2::operator==(other));
+}
+
 /*
     PRINTS OUTPUT
     Vector2 a = xi + yj;
@@ -78,4 +99,71 @@ bool Vector2::operator==(const Vector2 &other) const
 std::ostream &operator<<(std::ostream &stream, const Vector2 &other)
 {
     stream << "(" << other.X_COMPONENT << "," << other.Y_COMPONENT << ")";
+}
+
+/*
+    ZERO OR NULL VECTOR
+    A vector whose magnitude is Zero and direcction less
+*/
+Vector2 Vector2::zeroVector()
+{
+
+    this->X_COMPONENT = 0;
+    this->Y_COMPONENT = 0;
+
+    return *this;
+}
+
+/*
+    MAGNITUDE OF THE VECTOR
+    Magnitude = sqrt(x^2 + y^2)
+*/
+double Vector2::magnitudeOfVector()
+{
+    this->MAGNITUDE = sqrt(pow(this->X_COMPONENT, 2) + pow(this->Y_COMPONENT, 2));
+    return this->MAGNITUDE;
+}
+
+/*
+    UNIT VECTOR
+    unitVector = vector/magnitude
+*/
+Vector2 Vector2::unitVector()
+{
+    this->MAGNITUDE = magnitudeOfVector();
+    Vector2 unitVector = *this / this->MAGNITUDE;
+    return unitVector;
+}
+
+/*
+    DISTANCE BETWEEN 2 VECTORS
+    a = x1i + y1j
+    b = x2i + y2j
+    distance = mag(a-b)
+    distance = sqrt((x2-x1)^2 + (y2-y1)^2);
+*/
+double distanceBwVectors(Vector2 vector1, Vector2 vector2)
+{
+    return sqrt(pow((vector1.X_COMPONENT - vector2.X_COMPONENT), 2 + (vector1.Y_COMPONENT - vector2.Y_COMPONENT)));
+}
+
+/*
+    DIRECTION OF VECTOR
+    a = xi + yj
+    direction(radins) = atan(y/x)
+*/
+double Vector2::directionOfVector()
+{
+    this->DIRECTION = atan(this->Y_COMPONENT / this->X_COMPONENT);
+    return this->DIRECTION;
+}
+
+/*
+    ANGLE BETWEEN TWO VECTORS
+    a, b are 2 vectors
+    angle = arcos(a.b/|a|.|b|)
+*/
+double angleBwVectors(Vector2 vector1, Vector2 vector2)
+{
+    return acos((vector1 * vector2) / (vector1.MAGNITUDE * vector2.MAGNITUDE));
 }
--- includes/vector2.h
+++ includes/vector2.h
@@ -8,31 +8,46 @@
 class Vector2
 {
 
-private:
+public:
     /*
-        vector = xi+yj
-        Variables:
-            x = X_COMPONENT
-            y = Y_COMPONENT
+    vector = xi+yj
+    Variables:
+        x = X_COMPONENT
+        y = Y_COMPONENT
+        MAGNITUDE = mag(vector)
     */
     double X_COMPONENT;
     double Y_COMPONENT;
+    double MAGNITUDE;
+    double DIRECTION;
 
-public:
-    // constructor'
-    Vector2() {}
-    Vector2(double X_IN, double Y_IN) : X_COMPONENT(X_IN), Y_COMPONENT(Y_IN) {}
+    // constructor
+    Vector2() : X_COMPONENT(0), Y_COMPONENT(0), MAGNITUDE(0), DIRECTION(0) {} // default null or zero vector
+    Vector2(double X_IN, double Y_IN) : X_COMPONENT(X_IN), Y_COMPONENT(Y_IN), MAGNITUDE(magnitudeOfVector()), DIRECTION(directionOfVector()) {}
 
     // deconstructor
     ~Vector2() {}
 
     // Operations on vector
+    // Overload funtions
     Vector2 operator+(const Vector2 &other);     // Addition
     Vector2 operator-(const Vector2 &other);     // Subraction
+    Vector2 operator/(double Magnitude);         // Scalar divison of vector
     bool operator==(const Vector2 &other) const; // Checks Equality
+    bool operator!=(const Vector2 &other) const; // Checks for InEquality
     double operator*(const Vector2 &other);      // Multiplication
-    // Vector2 operator=(const Vector2 &other);  // Assigning Values
-    friend std::ostream &operator<<(std::ostream &stream, const Vector2 &other);
+
+    // Custom functions
+    Vector2 zeroVector();       // Converts into null/zero vector
+    double magnitudeOfVector(); // Calculates magnitude of vector
+    Vector2 unitVector();       // Claculates Unit Vector
+    double directionOfVector(); // Gives direcction of vector
+
+    // Friend Functions
+    friend std::ostream &
+    operator<<(std::ostream &stream, const Vector2 &other);
 };
 
 std::ostream &operator<<(std::ostream &stream, const Vector2 &other); // Printing Vectors2
+double distanceBwVectors(Vector2 vector1, Vector2 vector2);           // Calculates distance b/w 2 Vectors
+double angleBwVectors(Vector2 vector1, Vector2 vector2);              // Angle between vectors in radians
